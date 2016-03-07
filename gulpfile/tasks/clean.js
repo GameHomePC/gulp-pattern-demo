610975e6d4547addc13959785201ada5daedadfe
Yauhen Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2016-03-07 20:12:59 +0300

    fix

--- gulpfile/tasks/clean.js
+++ gulpfile/tasks/clean.js
@@ -5,7 +5,7 @@ if(!config.tasks.clean) return;
 var gulp = require('gulp'),
     rimraf = require('rimraf');
 
-var cleanFunc = function() {
+var cleanFunc = function(cb) {
     rimraf(config.tasks.clean, cb);
 };
 
